21a379bdf0e3b9f4834ed8649867dca0345f911f
Biruk Tassew <biruktassew21@gmail.com>
2023-04-19 14:32:57 +0300

    feat(backend): Add CRUD for comment

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,17 @@
 // package all routes in a single file
 
 import userRouter from "./user";
+import classRouter from "./class";
+import issueRouter from "./issue";
+import voteRouter from "./vote";
+import answerRouter from "./answer";
+import commentRouter from "./comment";
 
 export default {
-  userRouter
+  userRouter,
+  classRouter,
+  issueRouter,
+  voteRouter,
+  answerRouter,
+  commentRouter
 };
