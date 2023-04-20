2c6fb4c0da54298de732b6adf22315dd65f87348
Daniel Tefera Deti <danieltefera2211@gmail.com>
2023-04-20 10:00:18 +0300

    fix app.ts related bugs

--- src/app.ts
+++ src/app.ts
@@ -21,13 +21,24 @@ app.use(cors());
 
 app.use("/api/v1/user", routes.userRouter);
 app.use("/api/v1/class", routes.classRouter);
+app.use("/api/v1/issue", routes.issueRouter);
+app.use("/api/v1/vote", routes.voteRouter);
+app.use("/api/v1/answer", routes.answerRouter);
+app.use("/api/v1/comment", routes.commentRouter);
 
 
 app.get("/", (req, res) => {
-  return res.send("Welcome to school Hive Page!");
+  return res.send("Welcome to School Hive Page!");
 });
 
 // Error handling
 app.use(errorHandler);
 
+export const config = {
+  api: {
+    timeout: 30
+  }
+};
+
 export default app;
+
